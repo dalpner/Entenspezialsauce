f6ac348f8ff19564c5fd6d668ff0bc978a8872be
dalpner <74786569+dalpner@users.noreply.github.com>
2022-02-16 21:30:04 +0100

    add abstract command class test command class

---
+++ src/main/java/com/github/Discordbot.java
@@ -0,0 +1,12 @@
+package com.github;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Discordbot {
+    public static void main(String[] arguments) throws Exception
+    {
+        JDA api = JDABuilder.createDefault("OTQzNTc3NjU3MDg0MzY2ODc5.Yg1FLA.EmGDinG9IR3dhlLtnrf59G6yayU").build();
+        api.addEventListener(new EntenListener());
+    }
+}
---
+++ src/main/java/commands/Pong.java
@@ -0,0 +1,29 @@
+package commands;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class Pong extends Command{
+
+    public Pong() {
+        super("ping", "p","EntenspezialsaucePing", "sendNudes");
+    }
+
+    @Override
+    public void invoke(MessageReceivedEvent event, String... arguments) {
+        var channel = event.getChannel();
+        if(boolIsInt(arguments[0]))
+            for (int i = 0; i < Integer.parseInt(arguments[0]); i++){
+                channel.sendMessage("Pong!").queue();
+            }
+    }
+
+    private boolean boolIsInt(String arg){
+        try{
+            Integer.valueOf(arg);
+            return true;
+        }catch (NumberFormatException nfe){
+            System.out.println("Hahahaha da war wer inkompetent");
+            return false;
+        }
+    }
+}
