09a08821e50e74be695171005ad414bdf492d1d8
dalpner <74786569+dalpner@users.noreply.github.com>
2022-02-17 20:11:37 +0100

    add command GroupManager

---
+++ src/main/java/commands/GroupManager.java
@@ -0,0 +1,20 @@
+package commands;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class GroupManager extends Command{
+    public GroupManager(String invoker, String... alias) {
+        super(invoker, alias);
+    }
+
+    @Override
+    public void invoke(MessageReceivedEvent event, String... arguments) {
+            var channel = event.getChannel();
+
+    }
+
+    public String getMessage(){
+
+        return "";
+    }
+}
