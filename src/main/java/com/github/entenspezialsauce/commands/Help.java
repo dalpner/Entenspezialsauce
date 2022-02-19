c8ebb18ea378c02913432552a71cb067ca720e3d
dalpner <74786569+dalpner@users.noreply.github.com>
2022-02-19 12:43:58 +0100

    feat(commands): add help command

---
+++ src/main/java/com/github/entenspezialsauce/commands/Help.java
@@ -0,0 +1,33 @@
+package com.github.entenspezialsauce.commands;
+
+import com.github.entenspezialsauce.EntenListener;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+
+public class Help extends Command{
+
+    public Help() {
+        super("help", "h", "imstuck");
+    }
+
+    @Override
+    public void invoke(MessageReceivedEvent event, String... arguments) {
+        EmbedBuilder helpBuilder = new EmbedBuilder();
+        MessageChannel channel = event.getChannel();
+        helpBuilder.setTitle("Oh no... you don't know how this works? Let me show you.", null);
+        helpBuilder.setColor(new Color(154, 61, 21));
+        var entenListener = new EntenListener();
+        var commandList = entenListener.getCommands();
+        int counter = 1;
+        StringBuilder helpString = new StringBuilder();
+        for (Command commands:commandList) {
+            helpString.append(counter++).append(": ").append(entenListener.getPrefix()).append(commands.getInvoker()).append("\n");
+            helpString.append("Hier könnte ihre Werbung stehen").append("\n");
+        }
+        helpBuilder.setDescription(helpString.toString());
+        channel.sendMessageEmbeds(helpBuilder.build()).queue();
+    }
+}
