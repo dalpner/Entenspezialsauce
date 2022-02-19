938caa71cb60295602e7fcc7d6353f469bedbe8e
dalpner <74786569+dalpner@users.noreply.github.com>
2022-02-19 17:58:02 +0100

    feat: catch all pings over 10

--- src/main/java/com/github/entenspezialsauce/commands/PongCommand.java
+++ src/main/java/com/github/entenspezialsauce/commands/PongCommand.java
@@ -2,16 +2,29 @@ package com.github.entenspezialsauce.commands;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+
 public class PongCommand extends Command{
+
+    private final String gifUrl = "https://c.tenor.com/WqFUaRFxipoAAAAC/duck-duckit.gif";
 
     public PongCommand() {
         super("ping", "p","EntenspezialsaucePing", "sendNudes");
     }
 
+
     @Override
     public void invoke(MessageReceivedEvent event, String... arguments) {
         var channel = event.getChannel();
         if(this.boolIsInt(arguments[0]))
+            if (Integer.parseInt(arguments[0]) > 10){
+                channel.sendMessage("We don't do this here.").queue();
+                channel.sendMessage(this.gifUrl).queue();
+                return;
+            }
             for (int i = 0; i < Integer.parseInt(arguments[0]); i++){
                 channel.sendMessage("Pong!").queue();
             }
