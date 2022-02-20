77f1f77c88c1de4791412d26a001739e891e0556
dalpner <74786569+dalpner@users.noreply.github.com>
2022-02-20 18:37:23 +0100

    feat: add NumberEmotes

---
+++ src/main/java/com/github/entenspezialsauce/NumberEmotes.java
@@ -0,0 +1,19 @@
+package com.github.entenspezialsauce;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberEmotes {
+    public static final Map<Integer, String> emoteMap = Map.of(
+            1, ":one:",
+            2, ":two:",
+            3, ":three:",
+            4, ":four:",
+            5, ":five:",
+            6, ":six:",
+            7, ":seven:",
+            8, ":eight:",
+            9, ":nine:",
+            10, ":keycap_ten:"
+    );
+}
