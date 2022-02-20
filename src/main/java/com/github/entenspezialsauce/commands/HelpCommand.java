91890a140d645469ce2f335121dfa50768ab6c8c
dalpner <74786569+dalpner@users.noreply.github.com>
2022-02-20 15:01:52 +0100

    feat: description in HelpCommand

--- src/main/java/com/github/entenspezialsauce/commands/HelpCommand.java
+++ src/main/java/com/github/entenspezialsauce/commands/HelpCommand.java
@@ -11,20 +11,16 @@ import java.awt.*;
 public class HelpCommand extends Command {
 
     @Override
-    public void invoke(MessageReceivedEvent event, String... arguments) {
+    public void invoke(MessageReceivedEvent event, String... arguments){
         EmbedBuilder helpBuilder = new EmbedBuilder();
         MessageChannel channel = event.getChannel();
         helpBuilder.setTitle("Oh no... you don't know how this works? Let me show you.", null);
         helpBuilder.setColor(new Color(154, 61, 21));
         var entenListener = new EntenListener();
         var commandList = entenListener.getCommands();
-        int counter = 1;
-        StringBuilder helpString = new StringBuilder();
         for (Command commands : commandList) {
-            helpString.append(counter++).append(": ").append(entenListener.getPrefix()).append(commands.getInvoker()).append("\n");
-            helpString.append("Hier könnte ihre Werbung stehen").append("\n");
+            helpBuilder.addField(commands.getInvoker(), commands.getDescription(), false);
         }
-        helpBuilder.setDescription(helpString.toString());
         channel.sendMessageEmbeds(helpBuilder.build()).queue();
     }
 }
